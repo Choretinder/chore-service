153bac37caa01048fc94309fac6912008f405ebb
Jacob Jerrell <86279411+jjerrell@users.noreply.github.com>
2024-09-16 22:20:14 -0500

    Configure spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,35 @@ plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
     alias(libs.plugins.kotlinJvm) apply false
+    alias(libs.plugins.spotless)
+}
+
+subprojects {
+    apply(plugin = rootProject.libs.plugins.spotless.get().pluginId)
+
+    configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+        kotlin {
+            target("**/*.kt")
+            targetExclude("${layout.buildDirectory}/**/*.kt")
+            ktfmt()
+                .kotlinlangStyle()
+            licenseHeaderFile(rootProject.file("build-support/copyright.kt"))
+            trimTrailingWhitespace()
+            endWithNewline()
+        }
+        kotlinGradle {
+            target("**/*.gradle.kts")
+            targetExclude("${layout.buildDirectory}/**/*.gradle.kts")
+            ktfmt()
+                .kotlinlangStyle()
+        }
+        format("xml") {
+            target("**/*.xml")
+            targetExclude("**/build/**/*.xml")
+            // Look for the first XML tag that isn't a comment (<!--) or the xml declaration (<?xml)
+            licenseHeaderFile(rootProject.file("build-support/copyright.xml"), "(<[^!?])")
+            trimTrailingWhitespace()
+            endWithNewline()
+        }
+    }
 }
