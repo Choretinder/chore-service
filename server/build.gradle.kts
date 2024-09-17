83beebe1d6aa2101e88b192db3e4a6d078134fed
Jacob Jerrell <86279411+jjerrell@users.noreply.github.com>
2024-09-16 22:20:46 -0500

    Runs spotless

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -5,14 +5,16 @@ plugins {
 }
 
 group = "app.jjerrell.choretender.service"
+
 version = "1.0.0"
+
 application {
     mainClass.set("app.jjerrell.choretender.service.ApplicationKt")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=${extra["io.ktor.development"] ?: "false"}")
+    applicationDefaultJvmArgs =
+        listOf("-Dio.ktor.development=${extra["io.ktor.development"] ?: "false"}")
 }
 
 dependencies {
-    
     implementation(libs.logback)
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.netty)
