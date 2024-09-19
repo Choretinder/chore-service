aa110cedfb6e02e9f6bfcd99f04732056d0eeea5
Jacob Jerrell <86279411+jjerrell@users.noreply.github.com>
2024-09-18 20:18:22 -0500

    Add KoIn, Serialization, and Room dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
     alias(libs.plugins.kotlinJvm) apply false
+    alias(libs.plugins.ktor).apply(false)
+    alias(libs.plugins.ksp).apply(false)
+    alias(libs.plugins.roomDb).apply(false)
+    alias(libs.plugins.kotlinSerialization).apply(false)
     alias(libs.plugins.spotless)
 }
 
--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.kotlinJvm)
     alias(libs.plugins.ktor)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.roomDb)
+    alias(libs.plugins.kotlinSerialization)
     application
 }
 
@@ -16,8 +19,23 @@ application {
 
 dependencies {
     implementation(libs.logback)
+
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.netty)
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.server.json)
+
+    implementation(libs.koin.ktor)
+    implementation(libs.koin.logger)
+
+    implementation(libs.room.common)
+    implementation(libs.room.runtime)
+    implementation(libs.room.sqlite.core)
+    implementation(libs.room.sqlite.bundled)
+    ksp(libs.room.compiler)
+
     testImplementation(libs.ktor.server.tests)
     testImplementation(libs.kotlin.test.junit)
 }
+
+room { schemaDirectory("$projectDir/schemas") }
