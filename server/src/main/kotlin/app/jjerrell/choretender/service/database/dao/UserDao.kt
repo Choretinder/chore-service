e8eebb916c782979928591abcdfcc7602a965eb7
Jacob Jerrell <86279411+jjerrell@users.noreply.github.com>
2024-09-18 20:38:06 -0500

    Runs spotless

--- server/src/main/kotlin/app/jjerrell/choretender/service/database/dao/UserDao.kt
+++ server/src/main/kotlin/app/jjerrell/choretender/service/database/dao/UserDao.kt
@@ -1,3 +1,20 @@
+/*
+ * Choretinder
+ * Copyright (C) 2024  Jacob Jerrell (@jjerrell)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package app.jjerrell.choretender.service.database.dao
 
 import androidx.room.*
@@ -5,18 +22,13 @@ import app.jjerrell.choretender.service.database.entity.UserEntity
 
 @Dao
 interface UserDao {
-    @Insert(onConflict = OnConflictStrategy.ABORT)
-    suspend fun insertUser(user: UserEntity)
+    @Insert(onConflict = OnConflictStrategy.ABORT) suspend fun insertUser(user: UserEntity)
 
-    @Update
-    suspend fun updateUser(category: UserEntity)
+    @Update suspend fun updateUser(category: UserEntity)
 
-    @Query("SELECT * FROM userEntity")
-    suspend fun getAllUsers(): List<UserEntity>
+    @Query("SELECT * FROM userEntity") suspend fun getAllUsers(): List<UserEntity>
 
-    @Query("SELECT * FROM userEntity WHERE id = :id")
-    suspend fun getUserById(id: Int): UserEntity
+    @Query("SELECT * FROM userEntity WHERE id = :id") suspend fun getUserById(id: Int): UserEntity
 
-    @Query("DELETE FROM userEntity WHERE id = :id")
-    suspend fun deleteUserById(id: Int)
+    @Query("DELETE FROM userEntity WHERE id = :id") suspend fun deleteUserById(id: Int)
 }
