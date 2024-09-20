9b3668dacb4f5806d5ce21bb8f61dc31d4e748f4
Jacob Jerrell <86279411+jjerrell@users.noreply.github.com>
2024-09-19 20:13:12 -0500

    Organize libs

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -18,22 +18,31 @@ application {
 }
 
 dependencies {
-    implementation(libs.logback)
-
+    // Ktor Core
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.netty)
+    implementation(libs.logback)
+
+    // Auth & Sessions
+    implementation(libs.ktor.server.auth)
+    implementation(libs.ktor.server.sessions)
+
+    // Content Negotiation
     implementation(libs.ktor.server.content.negotiation)
     implementation(libs.ktor.server.json)
 
+    // DI
     implementation(libs.koin.ktor)
     implementation(libs.koin.logger)
 
+    // DB
     implementation(libs.room.common)
     implementation(libs.room.runtime)
     implementation(libs.room.sqlite.core)
     implementation(libs.room.sqlite.bundled)
     ksp(libs.room.compiler)
 
+    // Testing
     testImplementation(libs.ktor.server.tests)
     testImplementation(libs.kotlin.test.junit)
 }
